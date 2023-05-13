638ced3113727fa1a8dc188e854499abe79f869d
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-05-13 12:22:21 -0300

    First windows WIP.

--- MiddleEarthBank/Form1.Designer.cs
+++ MiddleEarthBank/Form1.Designer.cs
@@ -28,7 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Start));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.BtnApllyLoan = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(118, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Middle Earth Bank";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MiddleEarthBank.Properties.Resources.book;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(120, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Your name:";
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(259, 133);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(174, 22);
+            this.name.TabIndex = 3;
+            // 
+            // BtnApllyLoan
+            // 
+            this.BtnApllyLoan.BackColor = System.Drawing.Color.SaddleBrown;
+            this.BtnApllyLoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnApllyLoan.ForeColor = System.Drawing.Color.White;
+            this.BtnApllyLoan.Location = new System.Drawing.Point(259, 207);
+            this.BtnApllyLoan.Name = "BtnApllyLoan";
+            this.BtnApllyLoan.Size = new System.Drawing.Size(140, 63);
+            this.BtnApllyLoan.TabIndex = 4;
+            this.BtnApllyLoan.Text = "Apply for LOAN";
+            this.BtnApllyLoan.UseVisualStyleBackColor = false;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.BackColor = System.Drawing.Color.Chocolate;
+            this.BtnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnExit.ForeColor = System.Drawing.Color.White;
+            this.BtnExit.Location = new System.Drawing.Point(717, 389);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(75, 60);
+            this.BtnExit.TabIndex = 5;
+            this.BtnExit.Text = "EXIT";
+            this.BtnExit.UseVisualStyleBackColor = false;
             // 
             // Start
             // 
@@ -36,17 +108,32 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Peru;
             this.ClientSize = new System.Drawing.Size(804, 461);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnApllyLoan);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Start";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Middle Earth Bank";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Button BtnApllyLoan;
+        private System.Windows.Forms.Button BtnExit;
     }
 }
 
